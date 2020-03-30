39d694df5b8563f6913824bbe60071a90133350f
uchrissd <uchrissd@protonmail.com>
2020-03-29 21:06:29 -0700

    added skills, linkedin, github links

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -7,7 +7,35 @@ class Landing extends Component {
       <div style={{ width: "100", margin: "auto" }}>
         <Grid className="landing-grid">
           <Cell col={12}>
-            <img src="" />
+            <img
+              src={require("../images/chrismexicocity.jpg.PNG")}
+              alt="profile"
+              className="profile-image"
+            />
+            <div className="banner-text">
+              <h1>Full-stack Web Developer</h1>
+              <hr />
+
+              <p>HTML/CSS | JavaScript | React | NodeJS | Express | MongoDB</p>
+              <div className="social-links">
+                {/* LInkedIn */}
+                <a
+                  href="https://www.linkedin.com/in/christopherunderwoodsd/"
+                  rel="nopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-linkedin-square" aria-hidden="true" />
+                </a>
+                {/* Github */}
+                <a
+                  href="https://github.com/uchrissd"
+                  rel="nopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-github-square" aria-hidden="true" />
+                </a>
+              </div>
+            </div>
           </Cell>
         </Grid>
       </div>
