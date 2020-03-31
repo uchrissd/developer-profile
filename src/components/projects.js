c4c62592966ae5255cf1f04879df3d4a1bbc8c81
uchrissd <uchrissd@protonmail.com>
2020-03-30 21:56:12 -0700

    added more MDL component requirements, built card for first react project

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,10 +1,79 @@
 import React, { Component } from "react";
+import {
+  Tabs,
+  Tab,
+  Grid,
+  Cell,
+  Card,
+  CardTitle,
+  CardActions,
+  Button,
+  CardMenu,
+  IconButton
+} from "react-mdl";
 
 class Projects extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeTab: 0 };
+  }
+
+  toggleCategories() {
+    if (this.state.activeTab === 0) {
+      return (
+        <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+          <CardTitle
+            style={{
+              color: "#fff",
+              height: "176px",
+              background: "url() center / cover"
+            }}
+          >
+            React Project 1
+          </CardTitle>
+        </Card>
+      );
+    } else if (this.state.activeTab === 1) {
+      return (
+        <div>
+          <h1>This is MongoDB</h1>
+        </div>
+      );
+    } else if (this.state.activeTab === 2) {
+      return (
+        <div>
+          <h1>This is JavaScript</h1>
+        </div>
+      );
+    } else if (this.state.activeTab === 3) {
+      return (
+        <div>
+          <h1>This is JavaScript</h1>
+        </div>
+      );
+    }
+  }
+
   render() {
     return (
-      <div>
-        <h1>Projects</h1>
+      <div className="category-tabs">
+        <Tabs
+          activeTab={this.state.activeTab}
+          onChange={tabId => this.setState({ activeTab: tabId })}
+          ripple
+        >
+          <Tab>React</Tab>
+          <Tab>MongoDB</Tab>
+          <Tab>JavaScript</Tab>
+          <Tab>React</Tab>
+        </Tabs>
+        <section className="projects-grid">
+          <Grid className="projects-grid">
+            <Cell col={12}>
+              <div className="content"> {this.toggleCategories()}</div>
+            </Cell>
+          </Grid>
+        </section>
       </div>
     );
   }
