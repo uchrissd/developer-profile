988420fd069fe548616e30593d44dcff2daf405b
uchrissd <uchrissd@protonmail.com>
2020-04-04 21:25:47 -0700

    changed font color for the banner text

--- src/App.js
+++ src/App.js
@@ -8,16 +8,20 @@ function App() {
   return (
     <div className="demo-big-content">
       <Layout>
-        <Header style={{ backgroundColor: "#000000" }} title="Title" scroll>
-          <Navigation>
+        <Header
+          style={{ backgroundColor: "#000000", fontFamily: "Space Mono" }}
+          title="Chris Underwood"
+          scroll
+        >
+          <Navigation style={{ fontFamily: "Space Mono" }}>
             <Link to="/resume">Resume</Link>
             <Link to="/about">About</Link>
             <Link to="/projects">Projects</Link>
             <Link to="/contact">Contact</Link>
           </Navigation>
         </Header>
-        <Drawer title="Title">
-          <Navigation>
+        <Drawer title="Chris Underwood">
+          <Navigation style={{ fontFamily: "Space Mono" }}>
             <Link to="/resume">Resume</Link>
             <Link to="/about">About</Link>
             <Link to="/projects">Projects</Link>
