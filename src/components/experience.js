a26e9601c5176bf7f5e4c17bc77bf9d6c1f660e8
uchrissd <uchrissd@protonmail.com>
2020-04-07 22:48:39 -0700

    added second education info, rendered the info into the resume component, created experience component, rednered into the resume component, created the skills component

--- src/components/experience.js
+++ src/components/experience.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Grid, Cell } from "react-mdl";
-class Education extends Component {
+class Experience extends Component {
   render() {
     return (
       <Grid>
@@ -10,12 +10,12 @@ class Education extends Component {
           </p>
         </Cell>
         <Cell col={8}>
-          <h4 style={{ marginTop: "0px" }}>{this.props.schoolName}</h4>
-          <p>{this.props.schoolDescription}</p>
+          <h4 style={{ marginTop: "0px" }}>{this.props.jobName}</h4>
+          <p>{this.props.jobDescription}</p>
         </Cell>
       </Grid>
     );
   }
 }
 
-export default Education;
+export default Experience;
--- src/components/resume.js
+++ src/components/resume.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Grid, Cell } from "react-mdl";
 import Education from "./education";
+import Experience from "./experience";
 
 class Resume extends Component {
   render() {
@@ -33,7 +34,44 @@ class Resume extends Component {
           <Cell className="resume-right-col" col={8}>
             <h2>Education</h2>
 
-            <Education startYear={2008} />
+            <Education
+              startYear={2008}
+              endYear={2013}
+              schoolName={"San Francisco State University"}
+              schoolDescription={
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus et ligula varius, viverra dolor sollicitudin, rhoncus risus. Vestibulum rhoncus auctor nisl, at pharetra metus varius id. Phasellus pretium justo eget massa iaculis tincidunt quis ut justo. Etiam blandit turpis sed arcu fringilla luctus. Sed laoreet nunc non auctor finibus. Fusce placerat a augue id aliquet. Sed eget porta elit. Maecenas maximus fringilla enim, et rutrum mauris venenatis ut. Mauris lobortis sapien nec risus varius, et pellentesque augue consequat. Phasellus quis neque ac ante feugiat dapibus nec vitae ipsum. Maecenas ornare diam maximus, laoreet diam eu, euismod lorem. Aliquam vestibulum imperdiet velit, non semper elit mollis vitae. Vestibulum sollicitudin eget enim ac semper."
+              }
+            />
+
+            <Education
+              startYear={2019}
+              endYear={2020}
+              schoolName={"UC San Diego Extension"}
+              schoolDescription={
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus et ligula varius, viverra dolor sollicitudin, rhoncus risus. Vestibulum rhoncus auctor nisl, at pharetra metus varius id. Phasellus pretium justo eget massa iaculis tincidunt quis ut justo. Etiam blandit turpis sed arcu fringilla luctus. Sed laoreet nunc non auctor finibus. Fusce placerat a augue id aliquet. Sed eget porta elit. Maecenas maximus fringilla enim, et rutrum mauris venenatis ut. Mauris lobortis sapien nec risus varius, et pellentesque augue consequat. Phasellus quis neque ac ante feugiat dapibus nec vitae ipsum. Maecenas ornare diam maximus, laoreet diam eu, euismod lorem. Aliquam vestibulum imperdiet velit, non semper elit mollis vitae. Vestibulum sollicitudin eget enim ac semper."
+              }
+            />
+            <hr style={{ borderTop: "3px solid #c4b49a" }}></hr>
+            <h2>Experience</h2>
+            <Experience
+              startYear={2014}
+              endYear={2016}
+              jobName={"Digital Editor"}
+              jobDescription={
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ac ornare neque. Praesent in sem sit amet ante mollis consectetur in id tellus. Praesent eget dictum est. Donec aliquam vulputate sem, in tempus ligula pellentesque vel. Fusce sit amet tincidunt enim. Donec quis massa faucibus, tempor urna a, commodo nisi. Phasellus consectetur, turpis dapibus commodo ultricies, purus nibh sollicitudin mauris, sit amet viverra massa nibh quis eros. Mauris a scelerisque arcu, sit amet rhoncus erat."
+              }
+            ></Experience>
+
+            <Experience
+              startYear={2016}
+              endYear={"Current"}
+              jobName={"Web Producer"}
+              jobDescription={
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ac ornare neque. Praesent in sem sit amet ante mollis consectetur in id tellus. Praesent eget dictum est. Donec aliquam vulputate sem, in tempus ligula pellentesque vel. Fusce sit amet tincidunt enim. Donec quis massa faucibus, tempor urna a, commodo nisi. Phasellus consectetur, turpis dapibus commodo ultricies, purus nibh sollicitudin mauris, sit amet viverra massa nibh quis eros. Mauris a scelerisque arcu, sit amet rhoncus erat."
+              }
+            ></Experience>
+            <hr style={{ borderTop: "3px solid #c4b49a" }}></hr>
+            <h2>Skills</h2>
           </Cell>
         </Grid>
       </div>
