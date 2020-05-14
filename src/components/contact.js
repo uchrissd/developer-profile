422d9ef2ce348766470eb6474bda4d3a2539c6e0
uchrissd <uchrissd@protonmail.com>
2020-05-13 22:13:56 -0700

    updated the contact page, removed phone number, updated description

--- src/components/contact.js
+++ src/components/contact.js
@@ -14,13 +14,7 @@ class Contact extends Component {
               style={{ height: "250px" }}
             />
             <p style={{ width: "75%", margin: "auto", paddingTop: "1em" }}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
+              Want to reach me? Send me an email! It's the best way!
             </p>
           </Cell>
           <Cell col={6}>
@@ -28,14 +22,6 @@ class Contact extends Component {
             <hr />
             <div className="contact-list">
               <List>
-                <ListItem>
-                  <ListItemContent
-                    style={{ fontSize: "30px", fontFamily: "Exo" }}
-                  >
-                    (760) 822-7770
-                  </ListItemContent>
-                </ListItem>
-
                 <ListItem>
                   <ListItemContent
                     style={{ fontSize: "30px", fontFamily: "Exo" }}
